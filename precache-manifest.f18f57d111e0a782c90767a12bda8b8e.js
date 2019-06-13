9e8eeb5b527738e67df59a2bc6ca62bd558793b6
friggito <ornella.friggit@gmail.com>
2019-06-13 17:42:40 -0400

    Updates

---
+++ precache-manifest.f18f57d111e0a782c90767a12bda8b8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bc6ed9159b03ea2c973223a5157ff5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "946014f69fa5fbf8f3d8",
+    "url": "/portfolio/static/css/main.b6bdad56.chunk.css"
+  },
+  {
+    "revision": "baaaebc47656f2a2703e",
+    "url": "/portfolio/static/js/2.57bfc73d.chunk.js"
+  },
+  {
+    "revision": "946014f69fa5fbf8f3d8",
+    "url": "/portfolio/static/js/main.2bcd0afb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f16f1e31dfc1d5707a247cedc2714531",
+    "url": "/portfolio/static/media/model-leaning.f16f1e31.jpeg"
+  },
+  {
+    "revision": "05e818294002bb108c0358a880c135a8",
+    "url": "/portfolio/static/media/purple-computer.05e81829.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05ac5825c928c00ef0596306541f459f.js"
+  "/portfolio/precache-manifest.f18f57d111e0a782c90767a12bda8b8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
